009adde306951c63d42a64e85630e1c50829fad0
LeandroParisi <lepcbelisario@gmail.com>
2022-03-15 09:21:49 -0300

    SETUP: creates electron builder scripts and yml

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import 'reflect-metadata'
 import Container from 'typedi'
 import BotCore from './Domain/Flow/Startup/BotCore'
 import BotStartup from './Domain/Flow/Startup/BotStartup'
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
+import { autoUpdater } from 'electron-updater'
 const venom = require('venom-bot')
 
 class Main {
@@ -58,23 +59,82 @@ class Main {
     return bot  
   }
 
+
   private TesteElectron() : void {
+    let mainWindow : BrowserWindow
+
+    function createWindow () {
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+      })
+
+      mainWindow.loadFile('index.html')
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
+
+      mainWindow.once('ready-to-show', () => {
+        autoUpdater.checkForUpdatesAndNotify()
+      })
+    }
+    
     app.on('ready', () => {
-      console.log('App is ready')
+      createWindow()
+    })
+    
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+    
+    app.on('activate', function () {
+      if (mainWindow === null) {
+        createWindow()
+      }
+    })
 
-      const win = new BrowserWindow({
-        width: 600,
-        height: 400
-      })
-
-      const indexHTML = './index.html'
-
-      win.loadFile(indexHTML).then(() => {
-        // IMPLEMENT FANCY STUFF HERE
-      })
+    ipcMain.on('app_version', (event) => {
+      event.sender.send('app_version', { version: app.getVersion() })
+    })
+    
+    ipcMain.on('restart_app', () => {
+      autoUpdater.quitAndInstall()
+    })
+    
+    autoUpdater.on('update-available', () => {
+      mainWindow.webContents.send('update_available')
+    })
+    
+    autoUpdater.on('update-downloaded', () => {
+      mainWindow.webContents.send('update_downloaded')
     })
   }
 }
+
+
+  
+  
+  
+  // {
+  //   app.on('ready', () => {
+  //     console.log('App is ready')
+
+  //     const win = new BrowserWindow({
+  //       width: 600,
+  //       height: 400
+  //     })
+
+  //     const indexHTML = './index.html'
+
+  //     win.loadFile(indexHTML).then(() => {
+  //       // IMPLEMENT FANCY STUFF HERE
+  //     })
+  //   })
+  // }
+// }
 
 new Main().Run()
 // new Main().TESTE();
