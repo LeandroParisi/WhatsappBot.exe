b7316ece66778517bdc34562e2bd6ba38ab885f9
LeandroParisi <lepcbelisario@gmail.com>
2022-04-25 18:41:19 -0300

    feat: implements update spinner

--- src/Electron/EletronStartup.ts
+++ src/Electron/EletronStartup.ts
@@ -11,6 +11,7 @@ import ILoginSubscriber from './Interfaces/EventsSubscribers/ILoginSubscriber'
 @Service()
 export default class EletronStartup {
   private LoginSubscribers : Array<ILoginSubscriber> = []
+  private MainWindow : BrowserWindow
 
   /**
    *
@@ -24,6 +25,83 @@ export default class EletronStartup {
     this.LoadApp()
   }
 
+  private CreateWindow() {
+    this.MainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    })
+
+    this.MainWindow.loadFile('./WebPage/index.html')
+
+    this.MainWindow.on('closed', function () {
+      this.MainWindow = null
+    })
+
+    this.MainWindow.once('ready-to-show', () => {
+      try {
+        autoUpdater.checkForUpdatesAndNotify()
+      } catch (error) {
+        Logger.error("Error trying to check for updates.", error)
+      }
+    })
+  }
+
+  private InitializeApp() {
+    return async () => {
+      this.CreateWindow()
+      this.SetAutoUpdateEvents(this.MainWindow)
+      this.SetIpcEvents(this.MainWindow)
+
+      try {
+        const isLogged = await this.TryLogin()
+
+        if (!isLogged) {
+          this.MainWindow.webContents.send('request_login', 'Favor logar com sua credenciais.')
+        } else {
+          this.MainWindow.webContents.send('logged_in', 'Sua sessão é válida.')
+          for (const subscriber of this.LoginSubscribers) {
+            subscriber.ReceiveLogin()
+          }
+        }
+      } catch (error) {
+        const e = error as LoginError
+        Logger.error("Unable to log in", e)
+
+        this.MainWindow.webContents.send('invalid_token', error.message)
+
+        if (e.retryLogin) {
+          Logger.warn("Requesting log in retry", e)
+          this.MainWindow.webContents.send('request_login', error.message)
+        }
+      } 
+    }
+  }
+
+  private LoadApp() {
+    // Using closure to enforce this binding on this.InitializeApp
+    const initialize = this.InitializeApp()
+    
+    app.on('ready', async () => {
+      await initialize()
+    })
+    
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+    
+    app.on('activate', async () => {
+      if (this.MainWindow === null) {
+        await initialize()
+      }
+    })
+  }
+
   private async TryLogin() : Promise<boolean> {
     const hasToken = await this.UserDataHandler.HasToken()
 
@@ -35,110 +113,12 @@ export default class EletronStartup {
     }
   }
 
-  private LoadApp() : BrowserWindow {
-    let mainWindow : BrowserWindow
-
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
-        },
-      })
-
-      mainWindow.loadFile('./WebPage/index.html')
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
-
-      mainWindow.once('ready-to-show', () => {
-        try {
-          autoUpdater.checkForUpdatesAndNotify()
-        } catch (error) {
-          Logger.error("Error trying to check for updates.", error)
-        }
-      })
-    }
-
-    app.on('ready', async () => {
-      createWindow()
-      this.SetAutoUpdateEvents(mainWindow)
-      this.SetIpcEvents(mainWindow)
-
-      try {
-        const isLogged = await this.TryLogin()
-        console.log({isLogged})
-
-        if (!isLogged) {
-          mainWindow.webContents.send('request_login', 'Favor logar com sua credenciais.')
-        } else {
-          mainWindow.webContents.send('logged_in', 'Sua sessão é válida.')
-          for (const subscriber of this.LoginSubscribers) {
-            subscriber.ReceiveLogin()
-          }
-        }
-      } catch (error) {
-        const e = error as LoginError
-
-        mainWindow.webContents.send('invalid_token', error.message)
-
-        if (e.retryLogin) {
-          mainWindow.webContents.send('request_login', error.message)
-        }
-      }
-    })
-    
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') {
-        app.quit()
-      }
-    })
-    
-    app.on('activate', async () => {
-      if (mainWindow === null) {
-        createWindow()
-        this.SetAutoUpdateEvents(mainWindow)
-        this.SetIpcEvents(mainWindow)
-
-        try {
-          const isLogged = await this.TryLogin()
-          console.log({isLogged})
-  
-          if (!isLogged) {
-            mainWindow.webContents.send('request_login', "Favor logar com sua credenciais.")
-          } else {
-            mainWindow.webContents.send('logged_in', 'Sua sessão é válida.')
-            for (const subscriber of this.LoginSubscribers) {
-              subscriber.ReceiveLogin()
-            }
-          }
-        } catch (error) {
-          const e = error as LoginError
-  
-          mainWindow.webContents.send('invalid_token', error.message)
-  
-          if (e.retryLogin) {
-            mainWindow.webContents.send('request_login', error.message)
-          }
-        }
-      }
-    })
-
-
-    return mainWindow
-  }
-
   private SetIpcEvents(mainWindow : BrowserWindow) {
     ipcMain.on('app_version', (event) => {
-      console.log({version: app.getVersion() })
       event.sender.send('app_version', { version: app.getVersion() })
     })
 
     ipcMain.on('receive_input', async (_message, loginInfo : ILoginInfo) => {
-
       try {
         await this.UserDataHandler.Login(loginInfo)
         for (const subscriber of this.LoginSubscribers) {
